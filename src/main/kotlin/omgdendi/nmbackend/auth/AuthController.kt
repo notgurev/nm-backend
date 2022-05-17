291e6d3b4ca58ebb0834a65c905b8cdb41c192f2
nikita <notgurev@gmail.com>
2022-05-17 17:38:52 +0300

    implement simple AuthController

--- src/main/kotlin/omgdendi/nmbackend/auth/AuthController.kt
+++ src/main/kotlin/omgdendi/nmbackend/auth/AuthController.kt
@@ -1,5 +1,7 @@
 package omgdendi.nmbackend.auth
 
+import omgdendi.nmbackend.model.user.UserService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -7,18 +9,20 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/auth")
-class AuthController {
+class AuthController @Autowired constructor(
+    val userService: UserService
+) {
     data class RegisterRequest(val firstName: String, val lastName: String, val username: String, val password: String)
 
     @PostMapping("/register")
     fun register(@RequestBody x: RegisterRequest) {
-        TODO()
+        userService.createUser(x.firstName, x.lastName, x.username, x.password)
     }
 
     data class LoginRequest(val username: String, val password: String)
 
     @PostMapping("/login")
     fun login(@RequestBody x: LoginRequest) {
-        TODO()
+        // не используется
     }
 }
