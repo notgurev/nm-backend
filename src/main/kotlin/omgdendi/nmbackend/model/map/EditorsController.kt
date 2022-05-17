6e0124a92f801267de9feb2d1d081fbc4070b9b8
nikita <notgurev@gmail.com>
2022-05-17 19:46:46 +0300

    EditorsController.kt fixes

--- src/main/kotlin/omgdendi/nmbackend/model/map/EditorsController.kt
+++ src/main/kotlin/omgdendi/nmbackend/model/map/EditorsController.kt
@@ -1,9 +1,6 @@
 package omgdendi.nmbackend.model.map
 
-import omgdendi.nmbackend.common.MapId
-import omgdendi.nmbackend.common.StringMap
-import omgdendi.nmbackend.common.UserId
-import omgdendi.nmbackend.common.message
+import omgdendi.nmbackend.common.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
@@ -15,16 +12,16 @@ class EditorsController @Autowired constructor(
     data class AddEditorsRequest(val editors: List<UserId>)
 
     @PostMapping("/{mapId}/editors")
-    fun addEditorsToMap(@RequestBody request: AddEditorsRequest, @PathVariable mapId: MapId): StringMap {
+    fun addEditorsToMap(@RequestBody request: AddEditorsRequest, @PathVariable mapId: MapId): JSON {
         // todo auth
         mapService.addEditorsToMap(mapId, request.editors)
-        return message("editors added")
+        return message("editors added to map")
     }
 
     @DeleteMapping("/{mapId}/editors/{editorId}")
-    fun removeEditorFromMap(@PathVariable mapId: MapId, @PathVariable editorId: UserId): StringMap {
+    fun removeEditorFromMap(@PathVariable mapId: MapId, @PathVariable editorId: UserId): JSON {
         // todo auth
         mapService.removeEditorFromMap(mapId, editorId)
-        return message("editor removed")
+        return message("editor removed from map")
     }
 }
