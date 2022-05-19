af230667ee9324bed043e670d024587834a65594
nikita <notgurev@gmail.com>
2022-05-19 22:29:41 +0300

    cors (наверное это работает)

--- src/main/kotlin/omgdendi/nmbackend/NmBackendApplication.kt
+++ src/main/kotlin/omgdendi/nmbackend/NmBackendApplication.kt
@@ -2,10 +2,22 @@ package omgdendi.nmbackend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
 class NmBackendApplication
 
+@Configuration
+@EnableWebMvc
+class WebConfiguration : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**").allowedMethods("*")
+    }
+}
+
 fun main(args: Array<String>) {
-	runApplication<NmBackendApplication>(*args)
+    runApplication<NmBackendApplication>(*args)
 }
